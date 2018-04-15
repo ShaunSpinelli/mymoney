6e841fa7b9a75229a690aa9b9f98dcb8175631a1
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-04-15 19:37:35 +1000

    added stock simbol to new stock route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :stocks
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/stocks/new/:symbol', to:'stocks#new'
   get '/preview' , to: 'preview#index'
   get '/preview/show/:symbol' , to: 'preview#show'
   
